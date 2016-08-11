19cf5be883d4ced7c91d8d8637eb747ea9c2d13f
Pavel Filinkov <info@filinkov.ru>
2016-08-11 14:21:06 +0500

    Исправлены критические ошибки интеграции с Yandex.Market. Добавлена интеграция с новым сервисом Яндекса «Поиск для интернет магазинов» https://site.yandex.ru/catalogs/new.

--- modules/site_commerce_yml_export/site-commerce-yml-products.tpl.php
+++ modules/site_commerce_yml_export/site-commerce-yml-products.tpl.php
@@ -15,7 +15,7 @@
     <categories>
       <?php foreach ($categories as $term): ?>
       <?php $status = array(5, 10, 20, 30); ?>
-      <?php if (site_commerce_count_positions($term->tid, $status)): ?>
+      <?php if (site_commerce_count_positions($term->tid, $status, 0, TRUE)): ?>
       <category id="<?php echo $term->tid ?>"<?php if($term->parent): ?> parentId="<?php echo $term->parent ?>"<?php endif; ?>><?php echo site_commerce_yml_export_safe($term->name)?></category>
       <?php endif; ?>
       <?php endforeach; ?>
@@ -23,17 +23,23 @@
     <offers>
       <?php foreach ($pids as $pid):
         $position = site_commerce_position_load($pid, variable_get('site_commerce_imagecache_first', 'sc_first'));
-        $position_summary = theme('site_commerce_body', array('site_commerce' => $position->site_commerce, 'summary_allow' => TRUE, 'value_allow' => FALSE));
-        $position_summary = kvantstudio_summary($position_summary, 255, TRUE);
+        $position_summary = theme('site_commerce_body', array('site_commerce' => $position->site_commerce, 'summary_allow' => TRUE, 'value_allow' => TRUE));
+        $position_summary = kvantstudio_summary($position_summary, 175, TRUE);
 
         // Страна производитель.
         $country = "";
         if ($position->field_site_commerce_country) {
           $country = taxonomy_term_load($position->field_site_commerce_country);
         }
+
+        // Статус доступности товара для покупки.
+        $available = 'false';
+        if ($position->status == 10) {
+          $available = 'true';
+        }
       ?>
       <?php if ($position->field_site_commerce_cost_min || $position->field_site_commerce_cost): ?>
-      <offer id="<?php echo $position->pid; ?>" available="false">
+      <offer id="<?php echo $position->pid; ?>" available="<?php print $available; ?>">
       <url><?php echo url('site-commerce/' . $position->pid, array('absolute' => TRUE)); ?></url>
       <?php if ($position->field_site_commerce_cost_min): ?>
       <price><?php echo site_commerce_cost_format($position->field_site_commerce_cost_min); ?></price>
@@ -44,7 +50,7 @@
       <currencyId><?php echo variable_get('site_commerce_yandex_market_сurrency', 'RUR'); ?></currencyId>
       <categoryId><?php echo $position->tid; ?></categoryId>
       <?php if (!empty($position->image['url'])): ?><picture><?php echo $position->image['url']; ?></picture><?php endif; ?>
-      <store>false</store>
+      <store>true</store>
       <delivery>true</delivery>
       <name><?php echo site_commerce_yml_export_safe(check_plain($position->title)) ?></name>
       <description><?php echo site_commerce_yml_export_safe($position_summary) ?></description>
